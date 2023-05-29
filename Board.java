7d3def1f595cac260432e2c8c5021189565cdf9e
TairilKK <j.dobrosielski4@gmail.com>
2023-05-29 16:39:36 +0200

    Dodanie klasy Tile oraz zmiana klasy Board

--- Board.java
+++ Board.java
@@ -1,26 +1,45 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.util.*;
 import javax.swing.*;
-import javax.swing.border.Border;
-import java.awt.*;
 
-public class Board extends JLabel {
-    ImageIcon boardImg = new ImageIcon("src/img/board/SmallBoard.png");
-    String PlayerNick="Gracz";
-    boolean active;
+public class Board{
+    public JPanel Board = new JPanel();
+    private ArrayList<Tile> Fields = new ArrayList<Tile>();
 
-    Board(){
-        this.setText(PlayerNick);
-        this.setIcon(boardImg);
+    private int FieldSize;
+    private int BoardSize;
+    private int pos_x, pos_y;
+
+    Board(int x, int y, int BoardSize, boolean Big){
+        this.pos_x=x;
+        this.pos_y=y;
+
+        this.BoardSize=BoardSize;
+        this.FieldSize=36;
+        if(Big)
+            this.FieldSize=71;
+
+        Board.setBounds(0, 0, this.BoardSize*FieldSize, this.BoardSize*FieldSize);
+        Board.setLayout(null);
 
 
-        this.setHorizontalTextPosition(JLabel.CENTER);
-        this.setVerticalTextPosition(JLabel.TOP);
+        for(int i = 0; i < BoardSize; ++i){
+            for(int j = 0; j < BoardSize; ++j){
+                Fields.add(i*j+j, new Tile(Big, j*FieldSize,i*FieldSize));
+                Board.add(Fields.get(i*j+j).layeredPane);
+            }
+        }
 
+        Board.setLocation(pos_x, pos_y);
+        Board.setVisible(true);
+        Board.setOpaque(true);
+        Board.setBackground(Color.green);
+    }
 
-        //this.setBorder(BorderFactory.createLineBorder(Color.CYAN,3));
-        this.setIconTextGap(-5);
-
-
-        this.setFont(new Font("MV Boli", Font.PLAIN, 20));
-
-    };
+    public Tile getTile(int x, int y){
+        return Fields.get(x*y+y);
+    }
 }
--- main.java
+++ main.java
@@ -5,22 +5,19 @@ import java.io.IOException;
 public class main {
     public static void main(String[] args) throws IOException {
         MainFrame MyFrame = new MainFrame();
-        Board MainBoard = new Board();
-        MainBoard.setBounds(1080/2-336/2,380,336,368);
 
-        Board Player1Board = new Board();
-        Player1Board.setBounds(18,10,336,368);
+        Board MainBoard = new Board(1165/2-360/2,380, 10, false);
 
-        Board Player2Board = new Board();
-        Player2Board.setBounds(336+18*2,10,336,368);
+        Board Player1Board = new Board(18,10, 10, false);
 
-        Board Player3Board = new Board();
-        Player3Board.setBounds(336*2+18*3,10,336,368);
+        Board Player2Board = new Board(360+18*2,10, 10, false);
 
-        MyFrame.add(MainBoard);
-        MyFrame.add(Player1Board);
-        MyFrame.add(Player2Board);
-        MyFrame.add(Player3Board);
+        Board Player3Board = new Board(360*2+18*3,10, 10, false);
+
+        MyFrame.add(MainBoard.Board);
+        MyFrame.add(Player1Board.Board);
+        MyFrame.add(Player2Board.Board);
+        MyFrame.add(Player3Board.Board);
 
         BoardPanel ShipPanel = new BoardPanel();
         ShipPanel.setBounds(18,415,336,328);
