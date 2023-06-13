7cfe7b9465ed5f402cf6dfccff8f37c29e23eb56
Marta-Ambroziak <240625@edu.p.lodz.pl>
2023-06-13 14:29:52 +0200

    Zamiana JLabel przyciskow wyboru statkow na Jbutton

--- main.java
+++ main.java
@@ -9,11 +9,8 @@ public class main {
         MainBoard.setBounds(1080/2-336/2,380,336,368);
 
         Board_GUI mainBoardGUI = new Board_GUI(1165/2-360/2,380, 10, false);
-
         Board_GUI player1BoardGUI = new Board_GUI(18,10, 10, false);
-
         Board_GUI player2BoardGUI = new Board_GUI(360+18*2,10, 10, false);
-
         Board_GUI player3BoardGUI = new Board_GUI(360*2+18*3,10, 10, false);
 
         MyFrame.add(mainBoardGUI.Board);
@@ -21,7 +18,7 @@ public class main {
         MyFrame.add(player2BoardGUI.Board);
         MyFrame.add(player3BoardGUI.Board);
 
-        BoatPanel ShipPanel = new BoatPanel();
+        BoatPlacingPanel ShipPanel = new BoatPlacingPanel();
         ShipPanel.setBounds(18,415,336,328);
         MyFrame.add(ShipPanel);
 
