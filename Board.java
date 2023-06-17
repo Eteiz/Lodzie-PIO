e0389f27aaf9f34358d089fc9e839a596c5e81ea
Swiftlover13 <fokt.paula@gmail.com>
2023-06-17 10:20:32 +0200

    Dodanie metod: allBoatsSet, numBoatsToSet, checkAllBoatsShot w klasie Board, dodanie tablic Boats, ControlBoats potrzebnych do tych metod, skrócenie dugosci setBoats poprzez usuniecie powtarzajacego sie kodu oraz dodanie komentarzy ulatwiajacych zrozumienie kodu w klasah Boat, Board

--- Board.java
+++ Board.java
@@ -1,51 +1,41 @@
-import javax.swing.*;
-import javax.swing.border.Border;
-import java.awt.*;
-
 public class Board {
 
-    final int size = 10;
-    int Board[][] = new int[size][size];
+    private final int size = 10; //size of game board
+    public int Board[][] = new int[size][size]; //game board for placing boats
+    public int Boats[] = new int[4]; //array to know how many boats are set
+    private final int ControlBoats[] = new int[4]; //array to define how many boats of specified length are to be placed
 
+    Board()
+    {
+        this.ControlBoats[0] = 4; //game has 4 boats with length 1
+        this.ControlBoats[1] = 3; //game has 3 boats with length 2
+        this.ControlBoats[2] = 2; //game has 2 boats with length 3
+        this.ControlBoats[3] = 1; //game has 1 boat with length 4
+    }
+
+    //method to place boats
     public boolean setBoats(Boat boat) {
         if(!validateBoat(boat))
             return false;
         if(boat.length == 1)
             this.Board[boat.position.x][boat.position.y] = 1;
-        if(boat.length == 2) {
-            if(boat.direction == 0) {
-                for(int i = 0; i < 2; i++)
-                    this.Board[boat.position.x + i][boat.position.y] = 1;
-            }
-            else {
-                for(int i = 0; i < 2; i++)
-                    this.Board[boat.position.x][boat.position.y + i] = 1;
-            }
+        if(boat.direction == 0) {
+            for(int i = 0; i < boat.length; i++)
+                this.Board[boat.position.x + i][boat.position.y] = 1;
         }
-        if(boat.length == 3) {
-            if(boat.direction == 0) {
-                for(int i = 0; i < 3; i++)
-                    this.Board[boat.position.x + i][boat.position.y] = 1;
-            }
-            else {
-                for(int i = 0; i < 3; i++)
-                    this.Board[boat.position.x][boat.position.y + i] = 1;
-            }
+        else {
+            for(int i = 0; i < boat.length; i++)
+                this.Board[boat.position.x][boat.position.y + i] = 1;
         }
-        if(boat.length == 4) {
-            if(boat.direction == 0) {
-                for(int i = 0; i < 4; i++)
-                    this.Board[boat.position.x + i][boat.position.y] = 1;
-            }
-            else {
-                for(int i = 0; i < 4; i++)
-                    this.Board[boat.position.x][boat.position.y + i] = 1;
-            }
-        }
+        this.Boats[boat.length - 1] += 1;
         return true;
     }
+
+    //method to check if boat to place is valid
     public boolean validateBoat(Boat boat) {
         if(this.Board[boat.position.x][boat.position.y] == 1)
+            return false;
+        if(this.Boats[boat.length - 1] == this.ControlBoats[boat.length - 1])
             return false;
         int start = 0, end = 10;
         if(boat.direction == 0) {
@@ -86,6 +76,8 @@ public class Board {
         }
         return true;
     }
+
+    //metod to make shoots on the board
     public void shootBoat(Point shoot) {
         if(this.Board[shoot.x][shoot.y] == 0) {
             this.Board[shoot.x][shoot.y] = 2;
@@ -94,4 +86,39 @@ public class Board {
             this.Board[shoot.x][shoot.y] = -1;
         }
     }
+
+    //method to check if all boats are placed
+    public boolean allBoatsSet() {
+        for(int i = 0; i < this.ControlBoats.length; i++)
+        {
+            if(this.Boats[i] < this.ControlBoats[i])
+                return false;
+        }
+        return true;
+    }
+
+    //method to check how many boats are left to set
+    public int numBoatsToSet() {
+        if(allBoatsSet())
+           return 0;
+        int counter = 0;
+        for(int i = 0; i < this.ControlBoats.length; i++) {
+            if (this.Boats[i] < this.ControlBoats[i])
+                counter += this.ControlBoats[i] - this.Boats[i];
+        }
+        return counter;
+    }
+
+    //this checks if player has boats left on Board, and if can continue game
+    //returns true if no boat left
+    public boolean checkAllBoatsShot() {
+        for(int i = 0 ; i < size; i++){
+            for(int j = 0; j < size; i++) {
+                if(this.Board[i][j] == 1)
+                    return false;
+            }
+        }
+        return true;
+    }
+
 }
--- Boat.java
+++ Boat.java
@@ -1,7 +1,7 @@
 public class Boat {
-    public int length;
-    public int direction; //0 - horizontal down, 1 - vertical right
-    public Point position;
+    public int length; //length of boat
+    public int direction; //direction of boat placement: 0 - horizontal down, 1 - vertical right
+    public Point position; //position of Boat
     Boat(int length, int direction, Point position) {
         this.length = length;
         this.direction = direction;
