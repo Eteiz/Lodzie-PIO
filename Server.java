5531aaa90f5bcbc7d9acb5a0e8998700d465718a
vialomur <maxim44m@ukr.net>
2023-06-17 20:00:01 +0200

    to save work on shooting

--- Server.java
+++ Server.java
@@ -69,42 +69,232 @@ public class Server
             clients.get(i).idPlayer4 = idPlayer4;
         }
 
-
         // GAME LOOP
         boolean isEnd = false;
+        int numberOfLosers = 0;
         while (!isEnd)
         {
             for(int i=0;i<numberOfPlayers;i+=1)
             {
+                // Choosing on which board to shoot
+                int chosen = GetRandomWithoutOne(i);
+
+                System.out.println("Player: "+ i + " shoot player: " + chosen);
+
                 clients.get(i).isMyTurn = true;
-                while(clients.get(i).gui.mainBoardGUI.ChosenX == -1 && clients.get(i).gui.mainBoardGUI.ChosenY == -1)
+                if(i == 0)
                 {
-                    // Waiting to choose place to shoot
-                    continue;
+                    if(chosen == 1)
+                    {
+                        while(clients.get(i).gui.player1BoardGUI.ChosenX == -1 && clients.get(i).gui.player1BoardGUI.ChosenY == -1)
+                        {
+                            // Waiting to choose place to shoot
+                            continue;
+                        }
+                        clients.get(i).gui.player1BoardGUI.ResetBoardTiles();
+                    }
+                    if(chosen == 2)
+                    {
+                        while(clients.get(i).gui.player2BoardGUI.ChosenX == -1 && clients.get(i).gui.player2BoardGUI.ChosenY == -1)
+                        {
+                            // Waiting to choose place to shoot
+                            continue;
+                        }
+                        clients.get(i).gui.player2BoardGUI.ResetBoardTiles();
+                    }
+                    if(chosen == 3)
+                    {
+                        while(clients.get(i).gui.player3BoardGUI.ChosenX == -1 && clients.get(i).gui.player3BoardGUI.ChosenY == -1)
+                        {
+                            // Waiting to choose place to shoot
+                            continue;
+                        }
+                        clients.get(i).gui.player3BoardGUI.ResetBoardTiles();
+                    }
                 }
+                if(i == 1)
+                {
+                    if(chosen == 0)
+                    {
+                        while(clients.get(i).gui.player1BoardGUI.ChosenX == -1 && clients.get(i).gui.player1BoardGUI.ChosenY == -1)
+                        {
+                            // Waiting to choose place to shoot
+                            continue;
+                        }
+                        clients.get(i).gui.player1BoardGUI.ResetBoardTiles();
+                    }
+                    if(chosen == 2)
+                    {
+                        while(clients.get(i).gui.player2BoardGUI.ChosenX == -1 && clients.get(i).gui.player2BoardGUI.ChosenY == -1)
+                        {
+                            // Waiting to choose place to shoot
+                            continue;
+                        }
+                        clients.get(i).gui.player2BoardGUI.ResetBoardTiles();
+                    }
+                    if(chosen == 3)
+                    {
+                        while(clients.get(i).gui.player3BoardGUI.ChosenX == -1 && clients.get(i).gui.player3BoardGUI.ChosenY == -1)
+                        {
+                            // Waiting to choose place to shoot
+                            continue;
+                        }
+                        clients.get(i).gui.player3BoardGUI.ResetBoardTiles();
+                    }
+                }
+                if(i == 2)
+                {
+                    if(chosen == 0)
+                    {
+                        while(clients.get(i).gui.player1BoardGUI.ChosenX == -1 && clients.get(i).gui.player1BoardGUI.ChosenY == -1)
+                        {
+                            // Waiting to choose place to shoot
+                            continue;
+                        }
+                        clients.get(i).gui.player1BoardGUI.ResetBoardTiles();
+                    }
+                    if(chosen == 1)
+                    {
+                        while(clients.get(i).gui.player2BoardGUI.ChosenX == -1 && clients.get(i).gui.player2BoardGUI.ChosenY == -1)
+                        {
+                            // Waiting to choose place to shoot
+                            continue;
+                        }
+                        clients.get(i).gui.player2BoardGUI.ResetBoardTiles();
+                    }
+                    if(chosen == 3)
+                    {
+                        while(clients.get(i).gui.player3BoardGUI.ChosenX == -1 && clients.get(i).gui.player3BoardGUI.ChosenY == -1)
+                        {
+                            // Waiting to choose place to shoot
+                            continue;
+                        }
+                        clients.get(i).gui.player3BoardGUI.ResetBoardTiles();
+                    }
+                }
+                if(i == 3)
+                {
+                    if(chosen == 0)
+                    {
+                        while(clients.get(i).gui.player1BoardGUI.ChosenX == -1 && clients.get(i).gui.player1BoardGUI.ChosenY == -1)
+                        {
+                            // Waiting to choose place to shoot
+                            continue;
+                        }
+                        clients.get(i).gui.player1BoardGUI.ResetBoardTiles();
+                    }
+                    if(chosen == 1)
+                    {
+                        while(clients.get(i).gui.player2BoardGUI.ChosenX == -1 && clients.get(i).gui.player2BoardGUI.ChosenY == -1)
+                        {
+                            // Waiting to choose place to shoot
+                            continue;
+                        }
+                        clients.get(i).gui.player2BoardGUI.ResetBoardTiles();
+                    }
+                    if(chosen == 2)
+                    {
+                        while(clients.get(i).gui.player3BoardGUI.ChosenX == -1 && clients.get(i).gui.player3BoardGUI.ChosenY == -1)
+                        {
+                            // Waiting to choose place to shoot
+                            continue;
+                        }
+                        clients.get(i).gui.player3BoardGUI.ResetBoardTiles();
+                    }
+                }
+
                 int chosenX = clients.get(i).gui.mainBoardGUI.ChosenX;
                 int chosenY = clients.get(i).gui.mainBoardGUI.ChosenY;
                 Point pointToShoot = new Point(chosenX,chosenY);
-
-                clients.get(i).gui.mainBoardGUI.ResetBoardTiles();
-                // Choosing on which board to shoot
-                int chosen = GetRandomWithoutOne(i);
 
                 // shooting on that board
                 clients.get(chosen).gui.mainLogicBoard.shootBoat(pointToShoot);
 
                 // update board for one that was chosen to be shooted
                 Board mainLogicBoard = clients.get(chosen).gui.mainLogicBoard;
+                int resultValue = mainLogicBoard.Board[chosenX][chosenY];
                 clients.get(chosen).gui.mainBoardGUI.UpdateBoard(mainLogicBoard);
 
-                int idChosen = clients.get(chosen).id;
 
+                /*
+                player1 :
+                logicboard1 - player2
+                logicboard2 - player3
+                logicboard3 - player4
+
+                player2 :
+                logicboard1 - player1
+                logicboard2 - player3
+                logicboard3 - player4
+
+                player3 :
+                logicboard1 - player1
+                logicboard2 - player2
+                logicboard3 - player4
+
+                player4 :
+                logicboard1 - player1
+                logicboard2 - player2
+                logicboard3 - player3
+                */
                 // update other players logic boards
-                for(int ind=0;ind<numberOfPlayers;ind+=1)
+                if(i == 0) // player 1 shooted
                 {
-                    if(clients.get(i).id == idChosen)continue;
+                    if(chosen == 1){
+                        clients.get(0).gui.player1LogicBoard.Board[chosenX][chosenY] = resultValue;
+                        clients.get(2).gui.player2LogicBoard.Board[chosenX][chosenY] = resultValue;
+                        clients.get(3).gui.player2LogicBoard.Board[chosenX][chosenY] = resultValue;
+                    }
 
-                    clients.get(ind).gui.player1BoardGUI.UpdateBoard(mainLogicBoard);
+                    if(chosen == 2){
+                        clients.get(0).gui.player2LogicBoard.Board[chosenX][chosenY] = resultValue;
+                        clients.get(1).gui.player2LogicBoard.Board[chosenX][chosenY] = resultValue;
+                        clients.get(3).gui.player3LogicBoard.Board[chosenX][chosenY] = resultValue;
+                    }
+
+                    if(chosen == 3){
+                        clients.get(0).gui.player3LogicBoard.Board[chosenX][chosenY] = resultValue;
+                        clients.get(1).gui.player3LogicBoard.Board[chosenX][chosenY] = resultValue;
+                        clients.get(2).gui.player3LogicBoard.Board[chosenX][chosenY] = resultValue;
+                    }
+                }
+                if(i == 1) // player 2 shooted
+                {
+                    if(chosen == 0){
+                        clients.get(1).gui.player1LogicBoard.Board[chosenX][chosenY] = resultValue;
+                        clients.get(2).gui.player1LogicBoard.Board[chosenX][chosenY] = resultValue;
+                        clients.get(3).gui.player1LogicBoard.Board[chosenX][chosenY] = resultValue;
+                    }
+
+                    if(chosen == 2){
+                        clients.get(1).gui.player2LogicBoard.Board[chosenX][chosenY] = resultValue;
+                        clients.get(0).gui.player2LogicBoard.Board[chosenX][chosenY] = resultValue;
+                        clients.get(3).gui.player3LogicBoard.Board[chosenX][chosenY] = resultValue;
+                    }
+
+                    if(chosen == 3){
+                        clients.get(1).gui.player3LogicBoard.Board[chosenX][chosenY] = resultValue;
+                        clients.get(0).gui.player3LogicBoard.Board[chosenX][chosenY] = resultValue;
+                        clients.get(2).gui.player3LogicBoard.Board[chosenX][chosenY] = resultValue;
+                    }
+
+                }
+                if(i == 2)
+                {
+                    if(chosen == 0)clients.get(i).gui.player1LogicBoard.Board[chosenX][chosenY] = resultValue;
+
+                    if(chosen == 1)clients.get(i).gui.player2LogicBoard.Board[chosenX][chosenY] = resultValue;
+
+                    if(chosen == 3)clients.get(i).gui.player3LogicBoard.Board[chosenX][chosenY] = resultValue;
+
+                }
+                if(i == 3)
+                {
+                    if(chosen == 0)clients.get(i).gui.player1LogicBoard.Board[chosenX][chosenY] = resultValue;
+
+                    if(chosen == 1)clients.get(i).gui.player2LogicBoard.Board[chosenX][chosenY] = resultValue;
+
+                    if(chosen == 2)clients.get(i).gui.player3LogicBoard.Board[chosenX][chosenY] = resultValue;
                 }
 
             }
